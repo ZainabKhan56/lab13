340bae7927c65e285b20397fe7e5ee431df984e1
ZainabKhan56 <156169590+ZainabKhan56@users.noreply.github.com>
2024-01-17 22:38:54 +0500

    Add files via upload

---
+++ JavaApplication67.java
@@ -0,0 +1,9 @@
+
+package javaapplication67;
+public class JavaApplication67 {
+    public static void main(String[] args) {
+        System.out.println("Hello Javeria!");
+        System.out.println("Hello Zainab!");
+    }
+    
+}
